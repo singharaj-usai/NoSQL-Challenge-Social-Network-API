0c5fed5709e25083e5803fb13a7bfdab561e2bbe
singharaj usai <djjd22938@gmail.com>
2022-10-01 20:33:40 -0400

    server.js again

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
 const { connect } = require('mongoose');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(require());
 
 // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
@@ -9,3 +16,7 @@ connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+app.listen(PORT, () =>
+console.log(`API server for running on port ${PORT}!`)
+);
