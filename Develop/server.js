7a5213c4807eb5587e8e2c5faf9580ac778024f0
singharaj usai <djjd22938@gmail.com>
2022-10-02 12:25:07 -0400

    test

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,6 @@ const { connect } = require('mongoose');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
 app.use(require('./routes'));
 
