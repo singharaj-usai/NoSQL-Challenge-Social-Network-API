d0d97bf4a65b6f604e920661dd21aa6a8045d5a5
singharaj usai <djjd22938@gmail.com>
2022-10-01 21:17:25 -0400

    user.js models

---
+++ Develop/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const transactionSchema = new Schema({
+
+//username
+
+//String
+//Unique
+//Required
+//Trimmed
+  username: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: "Enter a name"
+  },
+//email
+
+//String
+//Required
+//Unique
+//Must match a valid email address (look into Mongoose's matching validation)
+
+  email: {
+    type: String,
+    required: "Enter an email",
+    unique: true,
+    match: [/.+@.+\..+/]//mongoose match validation
+
+  },
+  //thoughts
+
+//Array of _id values referencing the Thought model
+
+  thoughts: {
+    type: Date, //wait
+    ref: 'Thought',
+  },
+
+  //friends
+
+//Array of _id values referencing the User model (self-reference)
+friends: {
+  type: Date, //wait
+  ref: 'User',
+},
+
+});
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+module.exports = Transaction;
